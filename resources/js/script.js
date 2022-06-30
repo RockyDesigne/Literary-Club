3d1fa19de0f7c7ef7010fcff61bd842021427740
RockyDesigne <107836920+RockyDesigne@users.noreply.github.com>
2022-06-30 15:59:08 +0300

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,29 @@
+const getElement = () => {
+  let x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+const menuDown = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
